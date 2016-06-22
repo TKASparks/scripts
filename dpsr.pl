e811d03df783f39b2f0ba37da31943f4679b72b9
Travis Sparks <Travissparks97@live.com>
2016-06-22 16:21:32 -0400

    Functionality Changes
    Added the Move file option to move files from branch to branch, also
    tweaked the view option for viewing unique and differing files to work
    properly

--- dpsr.pl
+++ dpsr.pl
@@ -21,7 +21,8 @@ print "What would you like to do?\n";
 print "\n";	
 print "1. View files unique to branches.\n";
 print "2. View differing files.\n";
-print "3. Exit.\n";
+print "3. Move a file.\n";
+print "4. Exit.\n";
 
 print "\nEnter you choice:\n";
 
@@ -29,19 +30,47 @@ chomp(my $mchoice = <STDIN>);
 
 	if($mchoice eq '1'){
 	system(clear);
-	my $cmd = "cat $diff | grep 'Only'";
+	my $cmd = "grep 'differ' $choice";;
+	print("=======================================================================\n");
 	print(system("$cmd"));
+	print("=======================================================================\n");
 	}
 	elsif($mchoice eq '2'){
 	system(clear);
-	my $cmd2 = "cat '$choice' | grep 'differ'";
+	my $cmd2 = "grep 'Only in' $choice";
 	print("=======================================================================\n");
 	print(system("$cmd2"));
 	print("=======================================================================\n");
 	}
 	elsif($mchoice eq '3'){
+	sub prompt {
+        my ($query) = @_; #This takes a prompt string as an argument
+        local $| = 1; #this will activate autoflush so the prompt will show immediately after diff file is shown
+        print $query;
+        chomp(my $answer = <STDIN>);
+        return $answer;
+		}
+	
+	sub prompt_yn {
+        my($query) = @_;
+        my $answer = prompt("$query (Y/N): ");
+        return lc($answer) eq 'y';
+		}
+
+	if (prompt_yn("Do you want to move a file?")){
+        my $file = prompt("Enter the path to your file: \n");
+        my $dest = prompt("Where do you want to move $file?: \n");
+        use File::Copy;
+        move ($file, $dest) || die "\nThe file or destination does not exist.\n";
+        print "The file was moved successfully.\n";
+		}	
+	}
+	elsif($mchoice eq '4'){
 	system(clear);
 	print "Goodbye!\n";
 	die;
-	}	
+	}
+	
 }
+	
+
